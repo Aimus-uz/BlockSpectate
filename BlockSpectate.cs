8b7db512ad8301492d545d3e4258582bff9f3ff1
Aimus-uz <jasursvet@gmail.com>
2026-06-26 09:52:19 +0500

    Update BlockSpectate.cs

--- BlockSpectate.cs
+++ BlockSpectate.cs
@@ -1,25 +1,30 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Utils;
+using System.Collections.Generic;
 
 namespace BlockSpectate;
 
 public class BlockSpectate : BasePlugin
 {
     public override string ModuleName => "BlockSpectate";
-    public override string ModuleVersion => "1.0.0";
+    public override string ModuleVersion => "1.2.0";
     public override string ModuleAuthor => "ZAADROT.UZ";
-    public override string ModuleDescription => "Blocks spectator during live game, admins are allowed";
+    public override string ModuleDescription => "Players who join spectator mid-game skip current and next round respawn";
 
     private bool _isWarmup = true;
+
+    // Players who went to spectator during live game - they must wait 1 extra round
+    private HashSet<ulong> _spectatedThisRound = new();
+    private HashSet<ulong> _skipNextRound = new();
 
     public override void Load(bool hotReload)
     {
         RegisterEventHandler<EventWarmupEnd>(OnWarmupEnd);
         RegisterEventHandler<EventRoundStart>(OnRoundStart);
         RegisterEventHandler<EventPlayerTeam>(OnPlayerTeam);
+        RegisterEventHandler<EventPlayerSpawn>(OnPlayerSpawn, HookMode.Pre);
     }
 
     private HookResult OnWarmupEnd(EventWarmupEnd @event, GameEventInfo info)
@@ -32,19 +37,11 @@ public class BlockSpectate : BasePlugin
     {
         if (_isWarmup) return HookResult.Continue;
 
-        // Check all spectators at round start and move non-admins out
-        foreach (var player in Utilities.GetPlayers())
-        {
-            if (!player.IsValid || player.IsBot || player.IsHLTV) continue;
-            if (player.TeamNum != (int)CsTeam.Spectator) continue;
+        // Players who were in spectator last round now skip this round
+        foreach (var steamId in _spectatedThisRound)
+            _skipNextRound.Add(steamId);
 
-            if (!AdminManager.PlayerHasPermissions(player, "@css/generic"))
-            {
-                player.ChangeTeam(CsTeam.None);
-                player.PrintToChat($" \x01[\x04ZAADROT\x01] \x07Spectator недоступен во время игры!");
-            }
-        }
-
+        _spectatedThisRound.Clear();
         return HookResult.Continue;
     }
 
@@ -55,26 +52,46 @@ public class BlockSpectate : BasePlugin
         var player = @event.Userid;
         if (player == null || !player.IsValid || player.IsBot) return HookResult.Continue;
 
-        // Check if player is trying to join spectator
-        if (@event.Team != (int)CsTeam.Spectator) return HookResult.Continue;
-
-        // Allow admins
-        if (AdminManager.PlayerHasPermissions(player, "@css/generic"))
-            return HookResult.Continue;
-
-        // Block non-admins
-        info.DontBroadcast = true;
-
-        // Send back to unassigned / force team select
-        Server.NextFrame(() =>
+        // Player going TO spectator
+        if (@event.Team == (int)CsTeam.Spectator)
         {
-            if (player.IsValid)
-            {
-                player.ChangeTeam(CsTeam.None);
-                player.PrintToChat($" \x01[\x04ZAADROT\x01] \x07Spectator недоступен во время игры!");
-            }
-        });
+            // Admins can spectate freely
+            if (AdminManager.PlayerHasPermissions(player, "@css/generic"))
+                return HookResult.Continue;
 
-        return HookResult.Stop;
+            // Mark this player - they went to spec during live game
+            _spectatedThisRound.Add(player.SteamID);
+            player.PrintToChat($" \x01[\x04ZAADROT\x01] \x07Вы пропустите следующий раунд!");
+        }
+
+        // Player leaving spectator - remove from skip list after 1 round delay handled in spawn
+        return HookResult.Continue;
+    }
+
+    private HookResult OnPlayerSpawn(EventPlayerSpawn @event, GameEventInfo info)
+    {
+        if (_isWarmup) return HookResult.Continue;
+
+        var player = @event.Userid;
+        if (player == null || !player.IsValid || player.IsBot) return HookResult.Continue;
+
+        if (_skipNextRound.Contains(player.SteamID))
+        {
+            _skipNextRound.Remove(player.SteamID);
+
+            // Kill them immediately so they can't play this round
+            Server.NextFrame(() =>
+            {
+                if (player == null || !player.IsValid) return;
+                var pawn = player.PlayerPawn?.Value;
+                if (pawn != null && pawn.IsValid)
+                {
+                    pawn.CommitSuicide(false, true);
+                    player.PrintToChat($" \x01[\x04ZAADROT\x01] \x07Вы пропустили раунд за выход в spectator!");
+                }
+            });
+        }
+
+        return HookResult.Continue;
     }
 }
